9c05dd4ec21cfe0ca74fb0decad89ee4f7f573a1
kshamshAI <kshamsh40@gmail.com>
2026-05-14 14:28:08 +0000

    new file:   01matrixGraphs.py  Leetcode-542 	new file:   detect_cycleGraphs.py GeeksforGeeks Problem

---
+++ Graphs/01matrixGraphs.py
@@ -0,0 +1,50 @@
+'''542. 01 Matrix
+Solved
+Medium
+Given an m x n binary matrix mat, return the distance of the nearest 0 for each cell.
+The distance between two cells sharing a common edge is 1.
+Example 1:
+Input: mat = [[0,0,0],[0,1,0],[0,0,0]]
+Output: [[0,0,0],[0,1,0],[0,0,0]]
+Example 2:
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]]
+Output: [[0,0,0],[0,1,0],[1,2,1]]
+Constraints:
+m == mat.length
+n == mat[i].length
+1 <= m, n <= 104
+1 <= m * n <= 104
+mat[i][j] is either 0 or 1.
+There is at least one 0 in mat.'''
+from copy import deepcopy
+from collections import deque
+
+class Solution:
+    def updateMatrix(self, mat):
+        grid = deepcopy(mat)
+        row = len(grid)
+        col = len(grid[0])
+        visited =[[0 for _ in range(col)] for _ in range(row)]
+        distance =  [[0 for _ in range(col)] for _ in range(row)]
+        queue = deque()
+        for r in range(row):
+            for c in range(col):
+                if grid[r][c] == 0:
+                    queue.append([r,c,0])
+                    visited[r][c] = 1
+        while queue:
+            i,j,d= queue.popleft()
+            distance[i][j] = d
+            for x,y in [[1,0],[-1,0],[0,1],[0,-1]]:
+                new_i = i+x
+                new_j = j+y
+                if new_i < 0 or new_i >=row or new_j < 0 or new_j >= col:
+                    continue
+                if visited[new_i][new_j] == 1 :
+                    continue
+                queue.append([new_i,new_j,d+1])
+                visited[new_i][new_j] = 1
+        return distance 
+
+mat = [[0,0,0],[0,1,0],[1,1,1]]
+print(Solution().updateMatrix(mat))
---
+++ Graphs/detect_cycleGraphs.py
@@ -0,0 +1,60 @@
+
+from collections import deque
+class Solution:
+    def isCycle(self, V, edges):
+        # code here
+        visited = [0]*V
+        adj = [[] for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+       
+        for i in range(V):
+            if visited[i] == 1:
+                continue
+            queue = deque()
+            queue.append((i,-1))
+            visited[i] = 1
+            while queue:
+                node,parent = queue.popleft()
+                for n in adj[node]:
+                    if visited[n] == 0:
+                        visited[n] = 1
+                        queue.append((n,node))
+                    else:
+                        if n != parent:
+                            return True
+        return False
+
+    def dfs(self,adj,node,parent,visited,V):
+        visited[node] = 1
+        for n in adj[node]:
+            if visited[n] == 0:
+                result = self.dfs(adj,n,node,visited,V)
+                if result == True:
+                    return True
+        
+            elif visited[n] == 1 and  n != parent:
+                return True
+                
+        return False   
+                
+        
+    def isCycle2(self, V, edges):
+        # DFS
+        visited = [0]*V
+        adj = [[] for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        for i in range(V):
+            if visited[i] == 1:
+                continue
+            if self.dfs(adj,i,-1,visited,V) == True:
+                return True
+        return False
+V1 = 4
+edges1 = [[0, 1], [0, 2], [1, 2], [2, ]]
+V2 = 4
+edges2 = [[0, 1], [1, 2], [2, 3]]
+print(f'Output1: {Solution().isCycle2(V1,edges1)}, Output2: {Solution().isCycle2(V2,edges2)}')
