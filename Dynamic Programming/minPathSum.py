d435e8d3a27dbedba8a19e27ea21104edbd017e9
kshamshAI <kshamsh40@gmail.com>
2026-06-07 12:18:10 +0000

    new file:   minPathFallingSum.py Leetcode-931 	new file:   minPathSum.py Leetcode-64 	new file:   minPathsumTriangle.py Leetcode-120 	new file:   unique_pathI.py Leetcode-62 	new file:   unique_pathII.py Leetcode-63

---
+++ Dynamic Programming/minPathSum.py
@@ -0,0 +1,107 @@
+'''Leetcode------------64. Minimum Path Sum
+Solved
+Medium
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+Note: You can only move either down or right at any point in time.
+Example 1:
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+Example 2:
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
+Constraints:
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 200
+0 <= grid[i][j] <= 200'''
+from typing import List
+import sys
+class Solution:
+   
+    def minPathSum1(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        def solve(i,j,grid):
+            if i == 0 and j == 0:
+                return grid[i][j]
+            if i < 0 or j < 0:
+                return float('inf')
+            up_sum =  solve(i-1,j,grid)
+            left_sum = solve(i,j-1,grid)
+            return grid[i][j] + min(up_sum,left_sum)
+        return solve(m-1,n-1,grid)
+       
+    def minPathSum2(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1]*n for _ in range(m)]
+        def solve(i,j,grid,dp):
+            if i == 0 and j == 0:
+                return grid[i][j]
+            if i < 0 or j < 0:
+                return float('inf')
+            if dp[i][j] != -1:
+                return dp[i][j]
+            up_sum =  solve(i-1,j,grid,dp)
+            left_sum = solve(i,j-1,grid,dp)
+            dp[i][j] = grid[i][j] + min(up_sum,left_sum)
+            return dp[i][j]
+        return solve(m-1,n-1,grid,dp)
+    
+    def minPathSum2(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1]*n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i < 0 or j < 0:
+                    dp[i][j]= float('inf')
+                    continue
+                up_sum = dp[i-1][j]
+                left_sum = dp[i][j-1]
+                dp[i][j] = grid[i][j] + min(up_sum,left_sum)
+            return dp[m-1][n-1]
+    def minPathSum3(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1]*n for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                up_sum = dp[i-1][j] if i > 0 else float('inf')
+                left_sum = dp[i][j-1] if j>0 else float('inf')
+                dp[i][j] = grid[i][j] + min(up_sum,left_sum)
+        return dp[m-1][n-1]
+    
+    def minPathSum4(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        prev = [0]*n
+        for i in range(m):
+            curr = [0]*n
+            for j in range(n):
+                if i == 0 and j == 0:
+                    curr[0] = grid[0][0]
+                    continue
+                else:
+                    up_sum = prev[j] if i > 0 else float('inf')
+                    left_sum = curr[j-1] if j>0 else float('inf')
+                curr[j] = grid[i][j] + min(up_sum,left_sum)
+            prev = curr.copy()
+        return prev[n-1]
+
+
+      
+    
+       
+        
+grid = [[1,3,1],[1,5,1],[4,2,1]]
+print(Solution().minPathSum1(grid))
+print(Solution().minPathSum2(grid))
+print(Solution().minPathSum3(grid))
+
+ 
---
+++ Dynamic Programming/minPathsumTriangle.py
@@ -0,0 +1,86 @@
+'''Letcode----------------------120. Triangle
+Solved
+Medium
+Given a triangle array, return the minimum path sum from top to bottom.
+For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+Example 1:
+Input: triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+Output: 11
+Explanation: The triangle looks like:
+   2
+  3 4
+ 6 5 7
+4 1 8 3
+The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11 (underlined above).
+Example 2:
+Input: triangle = [[-10]]
+Output: -10
+Constraints:
+1 <= triangle.length <= 200
+triangle[0].length == 1
+triangle[i].length == triangle[i - 1].length + 1
+-104 <= triangle[i][j] <= 104
+'''
+from typing import List
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        def solve(i,j,triangle):
+            if i == len(triangle)-1:
+                return triangle[i][j]
+            
+            down =  triangle[i][j]+solve(i+1,j,triangle)
+            dia =  triangle[i][j]+solve(i+1,j+1,triangle)
+            return min(down,dia)
+        return solve(0,0,triangle)  
+    
+    
+    def minimumTotal1(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp =[[-1]*n for _ in range(n)]
+        def solve(i,j,triangle,dp):
+            if i == len(triangle)-1:
+                return triangle[i][j]
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
+            down =  triangle[i][j]+solve(i+1,j,triangle,dp)
+            dia =  triangle[i][j]+solve(i+1,j+1,triangle,dp)
+            dp[i][j]= min(down,dia)
+            return dp[i][j]
+        return solve(0,0,triangle,dp)   
+    
+      
+    def minimumTotal3(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp =[[-1]*n for _ in range(n)]
+        for k in range(n):
+            dp[n-1][k] = triangle[n-1][k]
+        for i in range(n-2,-1,-1):
+            for j in range(0,i+1):
+                
+                down =  triangle[i][j]+dp[i+1][j]
+                dia =  triangle[i][j]+dp[i+1][j+1]
+                dp[i][j]= min(down,dia)
+           
+        return dp[0][0] 
+    
+
+    def minimumTotal4(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        prev = [0]*n
+        for k in range(n):
+            prev[k] = triangle[n-1][k]
+        for i in range(n-2,-1,-1):
+            curr = [0]*(i+1)
+            for j in range(0,i+1):
+                
+                down =  triangle[i][j]+prev[j]
+                dia =  triangle[i][j]+prev[j+1]
+                curr[j]= min(down,dia)
+                
+            prev = curr.copy()
+           
+        return prev[0] 
+           
+triangle = [[2],[3,4],[6,5,7],[4,1,8,3]]
+print(Solution().minimumTotal4(triangle))
